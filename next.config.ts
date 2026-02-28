95971c2fe66461b7723ba66b317d31a802e20348
Hein <cantatasonatadev@gmail.com>
2026-02-28 22:42:29 +0700

    modify config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	turbopack: {
-		root: "/home/hein/personal/karat-book",
-	},
+
 };
 
 const wrappedConfig = withBundleAnalyzer({
